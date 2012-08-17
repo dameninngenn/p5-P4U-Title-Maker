b2699c6630566ac47524abecab62c1a879f3ec4d
dameninngenn <dameninngenn.owata@gmail.com>
2012-08-18 00:26:49 +0900

    initial commit

---
+++ lib/P4U/Title/Maker.pm
@@ -0,0 +1,32 @@
+package P4U::Title::Maker;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+P4U::Title::Maker -
+
+=head1 SYNOPSIS
+
+  use P4U::Title::Maker;
+
+=head1 DESCRIPTION
+
+P4U::Title::Maker is
+
+=head1 AUTHOR
+
+dameninngenn E<lt>dameninngenn.owata {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
